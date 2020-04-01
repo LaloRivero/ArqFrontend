6e59491f834561d00e3f41b4d2093b827aa48fd7
Eduardo Rivero <lalo@lalorivero.com>
2020-03-31 22:48:40 -0700

    format document

--- 07-Animaciones/Reto-Animation/index.js
+++ 07-Animaciones/Reto-Animation/index.js
@@ -1,52 +1,52 @@
-const planet = document.getElementById("planet");
-const mono = document.getElementById("mono");
-const play = document.getElementById("play");
-const pause = document.getElementById("pause");
-const reverse = document.getElementById("reverse");
+const planet = document.getElementById('planet');
+const mono = document.getElementById('mono');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const reverse = document.getElementById('reverse');
 
 const rotarPlaneta = planet.animate(
-  [
-    {
-      transform: "rotate(0)"
-    },
-    {
-      transform: "rotate(360deg)"
-    }
-  ],
-  {
-    duration: 10000,
-    easing: "linear",
-    iterations: Infinity
-  }
+	[
+		{
+			transform: 'rotate(0)'
+		},
+		{
+			transform: 'rotate(360deg)'
+		}
+	],
+	{
+		duration: 10000,
+		easing: 'linear',
+		iterations: Infinity
+	}
 );
 const brincar = mono.animate(
-  [
-    {
-      transform: "translateY(50px)"
-    },
-    {
-      transform: "translateY(0)"
-    }
-  ],
-  {
-    duration: 1000,
-    direction: "alternate",
-    easing: "ease",
-    iterations: Infinity,
-    fill: "forwards"
-  }
+	[
+		{
+			transform: 'translateY(50px)'
+		},
+		{
+			transform: 'translateY(0)'
+		}
+	],
+	{
+		duration: 1000,
+		direction: 'alternate',
+		easing: 'ease',
+		iterations: Infinity,
+		fill: 'forwards'
+	}
 );
 
-play.addEventListener("click", () => {
-  rotarPlaneta.play();
-  brincar.play();
+play.addEventListener('click', () => {
+	rotarPlaneta.play();
+	brincar.play();
 });
-pause.addEventListener("click", () => {
-  rotarPlaneta.pause();
-  brincar.pause();
+pause.addEventListener('click', () => {
+	rotarPlaneta.pause();
+	brincar.pause();
 });
-reverse.addEventListener("click", () => {
-  rotarPlaneta.reverse();
+reverse.addEventListener('click', () => {
+	rotarPlaneta.reverse();
 });
 
 rotarPlaneta.pause();
